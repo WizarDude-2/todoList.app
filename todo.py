1e0c120cf7ffb8be3d3308fa686f9fb824d39485
nateWinfield <natewinfield147@gmail.com>
2025-08-29 09:33:16 -0500

    loadTasks complete

--- todo.py
+++ todo.py
@@ -27,18 +27,28 @@ def saveTasks():
         file.write(str(tasks))
         file.write("\n")
         file.write(str(compList))
+    print("Tasks saved successfully")
 def loadTasks():
+    toggle = 0
     with open("todo.txt", "r") as file:
+        for line in file:
+            print(line)
+            if toggle == 0:
+                tasks = line
+                print(f"Tasks={tasks}")
+                toggle += 1
+            else:
+                compList = line
+                print(f"compList={compList}")
+    print("New tasks loaded successfully")
 # Demo flow (you can run this file directly: python todo.py)
 if __name__ == "__main__":
     addTask("Write code")
     addTask("Finish Cyber 201 assignment")
     addTask("Push code to GitHub")
     addTask("Buy Silksong")
-    viewTasks()
     delTasks(1)
-    viewTasks()
     markComplete(0)
+    loadTasks()
     viewTasks()
     viewComplete()
-    saveTasks()
